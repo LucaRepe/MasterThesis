2b3f785750a308f7c15cc2534eab4617e9d6fbcb
LucaRepe <lukarepe@live.it>
2022-11-17 16:38:20 +0100

    Updated disassemblerIDA script

--- headless_scripts/disassemblerIDA.py
+++ headless_scripts/disassemblerIDA.py
@@ -23,10 +23,10 @@ def run():
     for func in idautils.Functions():
         start = idc.get_func_attr(func, idc.FUNCATTR_START)
         end = idc.get_func_attr(func, idc.FUNCATTR_END)
-        #log("%s\t start: %x\t end: %x\n" % (idc.get_func_name(func), start, end))
         cur_addr = start
         while cur_addr <= end:
-            log(' '.join([to_hex(b) if b >= 0 else to_hex(unoverflow(b)) for b in idc.get_bytes(cur_addr, idc.get_item_size(cur_addr))]) + '\n')
+            log(' '.join([to_hex(b) if b >= 0 else to_hex(unoverflow(b)) for b in idc.get_bytes(cur_addr, idc.get_item_size(cur_addr))]).upper() + '\t\t')
+            log(idc.GetDisasm(cur_addr).upper() + '\n')
             cur_addr = idc.next_head(cur_addr, end)
 
     # if logging to a file, close it and exit IDA Pro

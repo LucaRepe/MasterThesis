e262d954875852aa0f142fb8ac0ad2dda9893951
LucaRepe <lukarepe@live.it>
2022-11-28 14:37:46 +0100

    Almost finished graph implementation on Angr

--- headless_scripts/disassemblerAngr.py
+++ headless_scripts/disassemblerAngr.py
@@ -3,8 +3,9 @@ import sys
 import re
 import networkx as nx
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
-from typing import List, Tuple
+from typing import List
 
 
 class BasicBlock:
@@ -12,30 +13,28 @@ class BasicBlock:
     list_bytes: List[List]
     list_instr: List[str]
     list_addr: List[int]
+    list_edges: List[int]
+    list_edge_attr: List[str]
     function_beginning: bool
     direct_fun_call: bool
     indirect_fun_call: bool
     direct_jump: bool
     indirect_jump: bool
+    has_return: bool
 
     def __repr__(self):
-        return f'{self.start_addr} {self.list_bytes} {self.list_instr} {self.list_addr}'
-
-
-class DummyNode:
-    addr: int
-
-    def __repr__(self):
-        return f'{self.addr}'
+        return f'{self.start_addr} {self.list_bytes} {self.list_instr} {self.list_addr} {self.list_edges} ' \
+               f'{self.list_edge_attr} '
 
 
 def run():
     f = open(sys.argv[2], 'w')
     base_addr = 0x100000
     p = angr.Project(sys.argv[1], auto_load_libs=False,
-                     load_options={'main_opts': {'base_addr': base_addr}})
+            load_options={'main_opts': {'base_addr': base_addr}})
     cfg = p.analyses.CFGFast()
     cfg.normalize()
+
     g = nx.DiGraph()
     for func_node in cfg.functions.values():
         for block in func_node.blocks:
@@ -43,11 +42,14 @@ def run():
             list_bytes = list()
             list_instr = list()
             list_addr = list()
+            list_edges = list()
+            list_edge_attr = list()
             func_beg = False
-            dir_fcall = False
-            indir_fcall = False
+            dir_call = False
+            indir_call = False
             dir_jump = False
             indir_jump = False
+            has_return = False
             if c.addr == func_node.addr:
                 func_beg = True
 
@@ -57,77 +59,91 @@ def run():
                 list_bytes.append(f' '.join(re.findall(r'.{1,2}', i.insn.bytes.hex())).upper())
                 list_instr.append(f'{i.mnemonic} {i.op_str}'.upper())
                 list_addr.append(f'{hex(i.address)}')
-            print(list_instr)
+            # print(list_instr)
 
             for instr in list_instr:
                 if 'JE' in instr or 'JNE' in instr or 'JMP' in instr:
                     jump_addr = instr.split(' ')[-1]
                     if jump_addr[-1] == ']':
-                        jump_addr = jump_addr.rstrip(jump_addr[-1])
-                        print("indir_jump")
+                        # jump_addr = jump_addr.rstrip(jump_addr[-1])
                         indir_jump = True
+                        list_edges.append("UnresolvableJumpTarget")
                     elif '0X' in jump_addr:
-                        print("dir_jump")
                         dir_jump = True
+                        list_edges.append(jump_addr.lower())
                     else:
-                        print("indir_jump")
                         indir_jump = True
-                    print(jump_addr.lower())
+                        list_edges.append("UnresolvableJumpTarget")
+                    list_edge_attr.append("Jump")
                 if 'CALL' in instr:
                     call_addr = instr.split(' ')[1]
                     if call_addr[-1] == ']':
-                        call_addr = call_addr.rstrip(jump_addr[-1])
-                        print("indir_fcall")
-                        indir_fcall = True
+                        # call_addr = call_addr.rstrip(jump_addr[-1])
+                        indir_call = True
+                        list_edges.append("UnresolvableCallTarget")
                     elif '0X' in call_addr:
-                        print("dir_fcall")
-                        dir_fcall = True
+                        dir_call = True
+                        list_edges.append(call_addr.lower())
                     else:
-                        print("indir_fcall")
-                        indir_fcall = True
-                    print(call_addr.lower())
+                        indir_call = True
+                        list_edges.append("UnresolvableCallTarget")
+                    list_edge_attr.append("Call")
+                if 'RET' in instr:
+                    has_return = True
 
             bb = BasicBlock()
             bb.start_addr = hex(block.addr)
             bb.list_bytes = list_bytes
             bb.list_instr = list_instr
             bb.list_addr = list_addr
+            bb.list_edges = list_edges
+            bb.list_edge_attr = list_edge_attr
             bb.function_beginning = func_beg
-            bb.direct_fun_call = dir_fcall
-            bb.indirect_fun_call = indir_fcall
+            bb.direct_fun_call = dir_call
+            bb.indirect_fun_call = indir_call
             bb.direct_jump = dir_jump
             bb.indirect_jump = indir_jump
-            g.add_node(bb.start_addr, instr=bb.list_instr, bytes=bb.list_bytes, addr=bb.list_addr,
-                       func_beg=bb.function_beginning, dir_fcall=bb.direct_fun_call, indir_fcall=bb.indirect_fun_call,
-                       dir_jump=bb.direct_jump, indir_jump=bb.indirect_jump)
+            bb.has_return = has_return
+            if len(list_instr) != 0:
+                g.add_node(bb.start_addr, instr=bb.list_instr, bytes=bb.list_bytes, addr=bb.list_addr,
+                           edges=bb.list_edges, edge_attr=bb.list_edge_attr, func_beg=bb.function_beginning,
+                           dir_call=bb.direct_fun_call, indir_call=bb.indirect_fun_call, dir_jump=bb.direct_jump,
+                           indir_jump=bb.indirect_jump, has_return=bb.has_return)
 
-    list_sort = sorted(list(g.nodes))[1:]
+    list_sorted = sorted(list(g.nodes))[1:]
     for node in sorted(list(g.nodes)):
-        print(g.nodes[node]['instr'])
-        print(g.nodes[node]['addr'])
-        for instr in list(g.nodes[node]['instr']):
-            if 'JE' in instr:
-                jump_addr = instr.split(' ')[1]
-                print(jump_addr.lower())
-                g.add_edge(node, jump_addr.lower())
-            if 'BND JMP' in instr:
-                length = len(instr)
-                if length == 16:
-                    jump_addr = instr.split(' ')[2]
-                    print(jump_addr.lower())
-                    g.add_edge(node, jump_addr.lower())
-            if 'CALL' in instr:
-                length = len(instr)
-                if length == 13:
-                    call_addr = instr.split(' ')[1]
-                    print(call_addr.lower())
-                    g.add_edge(node, call_addr.lower())
-        if list_sort:
-            g.add_edge(node, list_sort.pop(0))
+        # print(g.nodes[node]['instr'])
+        # print(g.nodes[node]['addr'])
+        if list_sorted:
+            if g.nodes[node]['has_return']:
+                list_sorted.pop(0)
+            else:
+                g.nodes[node]['edges'].append(list_sorted.pop(0))
+                g.nodes[node]['edge_attr'].append("Fallthrough")
+                for edge in g.nodes[node]['edges']:
+                    for attr in g.nodes[node]['edge_attr']:
+                        if attr == 'Jump':
+                            g.add_edge(node, edge, color='r')
+                        if attr == 'Call':
+                            g.add_edge(node, edge, color='b')
+                        if attr == 'Fallthrough':
+                            g.add_edge(node, edge, color='g')
 
-    
-    nx.draw_networkx(g, arrows=True)
+    colors = ['r', 'g', 'b']
+    nx.draw_networkx(g, edge_color=colors, arrows=True)
+
+    legend_elements = [
+        Line2D([0], [0], marker='o', color='r', label='Jump',markerfacecolor='r', markersize=15),
+        Line2D([0], [0], marker='o', color='b', label='Call',markerfacecolor='b', markersize=15),
+        Line2D([0], [0], marker='o', color='g', label='Fallthrough',markerfacecolor='g', markersize=15)        
+    ]
+    plt.legend(handles=legend_elements, loc='upper right')
     plt.show()
+
+
+if __name__ == '__main__':
+    run()
+
 
 
 if __name__ == '__main__':

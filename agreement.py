34ef5b3ff4134e780e978646d781d85c82b37a4c
LucaRepe <lukarepe@live.it>
2023-04-25 17:54:22 +0200

    Improved agreement file, uploaded pngs for graphs

--- agreement.py
+++ agreement.py
@@ -1,6 +1,18 @@
 import re
 import pickle
 import networkx as nx
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+
+def diff_graph_construction(agreement, purged):
+    graph_diff = nx.DiGraph()
+    graph_diff.add_edges_from(purged.edges() - agreement.edges())
+    for node in graph_diff.copy().nodes():
+        graph_diff.add_node(node, **purged.nodes[node])
+    for u, v, data in ((u, v, d) for u, v, d in purged.edges(data=True) if (u, v) not in agreement.edges()):
+        graph_diff.add_edge(u, v, **data)
+    return graph_diff
 
 
 def purge(graph, max_addr, min_addr):
@@ -19,7 +31,7 @@ def main():
     radare = pickle.load(open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/radare.p", "rb"))
 
     base_address = 0x400000
-    bbl_string = open('/home/luca/Scrivania/MasterThesis/Lab15-02.bbl').read()
+    bbl_string = open('/home/luca/Scrivania/MasterThesis/Lab15-01.bbl').read()
     bbl_list = re.findall('.{1,8}', bbl_string)
     pin_trace = set()
     for addr in bbl_list:
@@ -37,105 +49,55 @@ def main():
     ghidra_purged = purge(ghidra, int_max, int_min)
     ida_purged = purge(ida, int_max, int_min)
     radare_purged = purge(radare, int_max, int_min)    
-    
+
     common_edges = set(angr_purged.edges()).intersection(set(ghidra_purged.edges()), set(ida_purged.edges()), set(radare_purged.edges()))
+    agreement_graph = nx.DiGraph(common_edges)
+    angr_diff = diff_graph_construction(agreement_graph, angr_purged)
+    ghidra_diff = diff_graph_construction(agreement_graph, ghidra_purged)
+    ida_diff = diff_graph_construction(agreement_graph, ida_purged)
+    radare_diff = diff_graph_construction(agreement_graph, radare_purged)
 
-    angr_common = nx.DiGraph()
-    for edge in angr_purged.edges():
-        if edge in common_edges:
-            angr_common.add_edge(edge[0], edge[1])
+    print(f"{'agreement'} {agreement_graph}")
+    print(f"{'angr_diff'} {angr_diff}")
+    print(f"{'ghidra_diff'} {ghidra_diff}")
+    print(f"{'ida_diff'} {ida_diff}")
+    print(f"{'radare_diff'} {radare_diff}")
+    print('\n') 
 
-    ghidra_common = nx.DiGraph()
-    for edge in ghidra_purged.edges():
-        if edge in common_edges:
-            ghidra_common.add_edge(edge[0], edge[1])
-
-    ida_common = nx.DiGraph()
-    for edge in ida_purged.edges():
-        if edge in common_edges:
-            ida_common.add_edge(edge[0], edge[1])
-
-    radare_common = nx.DiGraph()
-    for edge in radare_purged.edges():
-        if edge in common_edges:
-            radare_common.add_edge(edge[0], edge[1])
-
-    angr_diff = nx.DiGraph()
-    for node in angr_purged:
-        angr_diff.add_node(node, **angr_purged.nodes[node])
-    angr_diff.remove_nodes_from(angr_common.nodes())
-    angr_diff.add_edges_from([edge for edge in angr_purged.edges() if edge[0] in angr_diff.nodes() and edge[1] in angr_diff.nodes()])
-
-    ghidra_diff = nx.DiGraph()
-    for node in ghidra_purged:
-        ghidra_diff.add_node(node, **ghidra_purged.nodes[node])
-    ghidra_diff.remove_nodes_from(ghidra_common.nodes())
-    ghidra_diff.add_edges_from([edge for edge in ghidra_purged.edges() if edge[0] in ghidra_diff.nodes() and edge[1] in ghidra_diff.nodes()])
-
-    ida_diff = nx.DiGraph()
-    for node in ida_purged:
-        ida_diff.add_node(node, **ida_purged.nodes[node])
-    ida_diff.remove_nodes_from(ida_common.nodes())
-    ida_diff.add_edges_from([edge for edge in ida_purged.edges() if edge[0] in ida_diff.nodes() and edge[1] in ida_diff.nodes()])
-
-    radare_diff = nx.DiGraph()
-    for node in radare_purged:
-        radare_diff.add_node(node, **radare_purged.nodes[node])
-    radare_diff.remove_nodes_from(radare_common.nodes())
-    radare_diff.add_edges_from([edge for edge in radare_purged.edges() if edge[0] in radare_diff.nodes() and edge[1] in radare_diff.nodes()])
-
-    pickle.dump(ghidra_diff, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ghidra_diff.p", "wb"))
-    pickle.dump(radare_diff, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/radare_diff.p", "wb"))
+    pickle.dump(agreement_graph, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/agreement.p", "wb"))
     pickle.dump(angr_diff, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/angr_diff.p", "wb"))
+    pickle.dump(ghidra_diff, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ghidra_diff.p", "wb"))
     pickle.dump(ida_diff, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ida_diff.p", "wb"))
-
-    common_edges_maj = set()
-    common_edges_maj |= ((set(angr_purged.edges()) & set(ghidra_purged.edges()) & set(ida_purged.edges())) |
-                    (set(angr_purged.edges()) & set(ghidra_purged.edges()) & set(radare_purged.edges())) |
-                    (set(angr_purged.edges()) & set(ida_purged.edges()) & set(radare_purged.edges())) |
-                    (set(ghidra_purged.edges()) & set(ida_purged.edges()) & set(radare_purged.edges())))
-
-    angr_common = nx.DiGraph()
-    for edge in angr_purged.edges():
-        if edge in common_edges_maj:
-            angr_common.add_edge(edge[0], edge[1])
-
-    ghidra_common = nx.DiGraph()
-    for edge in ghidra_purged.edges():
-        if edge in common_edges_maj:
-            ghidra_common.add_edge(edge[0], edge[1])
-
-    ida_common = nx.DiGraph()
-    for edge in ida_purged.edges():
-        if edge in common_edges_maj:
-            ida_common.add_edge(edge[0], edge[1])
-
-    radare_common = nx.DiGraph()
-    for edge in radare_purged.edges():
-        if edge in common_edges_maj:
-            radare_common.add_edge(edge[0], edge[1])
-
-    angr_diff_maj = angr_purged.copy()
-    angr_diff_maj.remove_nodes_from(angr_common.nodes())
-    angr_diff_maj.add_edges_from([edge for edge in angr_purged.edges() if edge[0] in angr_diff_maj.nodes() and edge[1] in angr_diff_maj.nodes()])
-
-    ghidra_diff_maj = ghidra_purged.copy()
-    ghidra_diff_maj.remove_nodes_from(ghidra_common.nodes())
-    ghidra_diff_maj.add_edges_from([edge for edge in ghidra_purged.edges() if edge[0] in ghidra_diff_maj.nodes() and edge[1] in ghidra_diff_maj.nodes()])
-
-    ida_diff_maj = ida_purged.copy()
-    ida_diff_maj.remove_nodes_from(ida_common.nodes())
-    ida_diff_maj.add_edges_from([edge for edge in ida_purged.edges() if edge[0] in ida_diff_maj.nodes() and edge[1] in ida_diff_maj.nodes()])
-
-    radare_diff_maj = radare_purged.copy()
-    radare_diff_maj.remove_nodes_from(radare_common.nodes())
-    radare_diff_maj.add_edges_from([edge for edge in radare_purged.edges() if edge[0] in radare_diff_maj.nodes() and edge[1] in radare_diff_maj.nodes()])
-
-    pickle.dump(ghidra_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ghidra_diff_maj.p", "wb"))
-    pickle.dump(radare_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/radare_diff_maj.p", "wb"))
-    pickle.dump(angr_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/angr_diff_maj.p", "wb"))
-    pickle.dump(ida_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ida_diff_maj.p", "wb"))
-
+    pickle.dump(radare_diff, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/radare_diff.p", "wb"))
     
+    common_edges_maj = set()
+    common_edges_maj |= ((set(angr_purged.edges) & set(ghidra_purged.edges) & set(ida_purged.edges)) |
+                 (set(angr_purged.edges) & set(ghidra_purged.edges) & set(radare_purged.edges)) |
+                 (set(angr_purged.edges) & set(ida_purged.edges) & set(radare_purged.edges)) |
+                 (set(ghidra_purged.edges) & set(ida_purged.edges) & set(radare_purged.edges)))
+
+
+    majority_graph = nx.DiGraph(common_edges_maj)
+    angr_diff_maj = diff_graph_construction(majority_graph, angr_purged)
+    ghidra_diff_maj = diff_graph_construction(majority_graph, ghidra_purged)
+    ida_diff_maj = diff_graph_construction(majority_graph, ida_purged)
+    radare_diff_maj = diff_graph_construction(majority_graph, radare_purged)
+
+    print(f"{'majority'} {majority_graph}")
+    print(f"{'angr_diff_maj'} {angr_diff_maj}")
+    print(f"{'ghidra_diff_maj'} {ghidra_diff_maj}")
+    print(f"{'ida_diff_maj'} {ida_diff_maj}")
+    print(f"{'radare_diff_maj'} {radare_diff_maj}")      
+
+    pickle.dump(majority_graph, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/majority.p", "wb"))
+    pickle.dump(angr_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/angr_diff_maj.p", "wb"))
+    pickle.dump(ghidra_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ghidra_diff_maj.p", "wb"))
+    pickle.dump(ida_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/ida_diff_maj.p", "wb"))
+    pickle.dump(radare_diff_maj, open("/home/luca/Scrivania/MasterThesis/Pickles/Complete/radare_diff_maj.p", "wb"))
+    
+    nx.draw_networkx(agreement_graph, with_labels=True)
+    plt.show()
+
+
 if __name__ == '__main__':
     main()
